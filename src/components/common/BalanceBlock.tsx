56224265bcc33f047cb97ac891c3e93003f4d1e7
Empty Set Squad <emptysetsquad@protonmail.com>
2020-09-05 00:22:06 -0700

    cleanup BalanceBlock formatting

--- src/components/common/BalanceBlock.tsx
+++ src/components/common/BalanceBlock.tsx
@@ -8,15 +8,61 @@ type BlanceBlockProps = {
   suffix?: string
 }
 
+function padZeros(nDecimalStr, len) {
+  while (nDecimalStr.length < len) {
+    nDecimalStr += '0'
+  }
+  return nDecimalStr;
+}
+
+function extractNum(nDecimalStr, start, len) {
+  return padZeros(nDecimalStr.substr(start, len), len);
+}
+
+function round(nDecimalStr, precision) {
+  const value = Math.round(parseInt(extractNum(nDecimalStr, 0, precision + 1)) / 10).toString()
+  console.log(value);
+  return value;
+}
+
+function sigfigs(nDecimalStr, sigfigs) {
+  let position = 0;
+  for (let i = 0; i < nDecimalStr.length; i++) {
+    if (nDecimalStr[i] !== '0') {
+      position = i;
+      break;
+    }
+  }
+
+  return nDecimalStr.substr(0, position) + round(nDecimalStr.substr(position), sigfigs)
+}
+
+function delineate(nIntegerStr) {
+  let result = "";
+  let group = 0;
+  for (let i = nIntegerStr.length - 1; i >= 0; i--) {
+    if (group == 3) {
+      result = "," + result;
+      group = 0;
+    }
+    result = nIntegerStr[i] + result;
+    group++;
+  }
+  return result;
+}
+
 function BalanceBlock({ asset, balance, suffix="" }: BlanceBlockProps) {
   let integer = '0';
   let digits = '0';
   if (new BigNumber(balance).gt(new BigNumber(0))) {
-    const str = new BigNumber(balance).toString();
+    const str = new BigNumber(balance).toFixed();
     const split = str.split('.');
-    integer = split[0];
+    integer = delineate(split[0]);
     digits = split.length > 1 ? str.split('.')[1] : '0';
-    digits = digits.length > 2 ? digits.substr(0, 2) : digits
+    digits = integer === "0" ? sigfigs(digits, 2) : round(digits, 2)
+    while (digits.length < 2) {
+      digits += '0';
+    }
   }
 
   return (
