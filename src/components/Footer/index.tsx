2d34dd10f2f8b3f402aa095b1f7bc5dad85d2d87
Empty Set Squad <emptysetsquad@protonmail.com>
2020-09-08 00:43:17 -0700

    add footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,54 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { LinkBase } from '@aragon/ui';
 
 function Footer({ theme } : { theme:string }) {
   const history = useHistory();
 
   return (
-    history.location.pathname.includes('/trade/')
-      ? <></>
-      : (
-        <div style={{
-          backgroundColor: theme === 'light' ? '#F8F8F8' : '#35425e',
-          textAlign: 'center',
-          padding: '17px',
-          position: 'fixed',
-          left: '0',
-          bottom: '0',
-          height: '50px',
-          width: '100%',
-          fontSize: '14px'
-        }}>
-          made with <span role="img" aria-labelledby="heartbreak">💔️</span> by the &#123;ess&#125;.
+    <>
+      <div style={{
+        backgroundColor: theme === 'light' ? '#F8F8F8' : '#35425e',
+        textAlign: 'center',
+        position: 'fixed',
+        left: '0',
+        bottom: '0',
+        height: 'auto',
+        width: '100%',
+        fontSize: '14px'
+      }}>
+        <div style={{maxWidth: '1100px', marginLeft: 'auto', marginRight: 'auto'}}>
+          <div style={{ padding: '2%', display: 'flex'}}>
+            <div style={{ width: '50%', textAlign: 'left' }}>
+              <FooterLink icon={<i className="fab fa-github"/>} onClick={() => window.location.href = "https://www.github.com/emptysetsquad/dollar"}/>
+              <FooterLink icon={<i className="fab fa-twitter"/>} onClick={() => window.location.href = "https://www.twitter.com/emptysetsquad"}/>
+              <FooterLink icon={<i className="fab fa-medium"/>} onClick={() => window.location.href = "https://www.medium.com/@emptysetsquad"}/>
+              <FooterLink icon={<i className="fab fa-telegram"/>} onClick={() => window.location.href = "https://www.t.me/emptysetdollar"}/>
+              <FooterLink icon={<i className="fab fa-discord"/>} onClick={() => window.location.href = "https://discord.gg/vPws9Vp"}/>
+            </div>
+            <div style={{ width: '50%', textAlign: 'right', height: '18px', marginTop: '15px', marginBottom: '15px'}}>
+              made with <span role="img" aria-labelledby="heartbreak">💔️</span> by the &#123;ess&#125;.
+            </div>
+          </div>
         </div>
-      )
+      </div>
+    </>
+  );
+}
+
+
+type FooterLinkProp = {
+  icon: any,
+  onClick: Function,
+}
+
+function FooterLink({
+  icon, onClick,
+}:FooterLinkProp) {
+  return (
+    <LinkBase onClick={onClick} style={{marginLeft: '8px', marginRight: '8px'}}>
+      <span style={{ fontSize: 32 }}>{icon}</span>
+    </LinkBase>
   );
 }
 
