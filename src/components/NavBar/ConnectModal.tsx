7e1cce3d3b6a16e38f020794388fde3263bcba36
Dave <dmaddock1@gmail.com>
2021-01-16 15:46:17 -0500

    Remember wallet info

--- src/components/NavBar/ConnectModal.tsx
+++ src/components/NavBar/ConnectModal.tsx
@@ -11,26 +11,18 @@ type ConnectModalProps = {
 function ConnectModal({
   visible, onClose, onConnect
 }:ConnectModalProps) {
-  const wallet = useWallet();
+  const { account, connect } = useWallet();
 
-  const connectMetamask = () => {
-    wallet.connect("injected");
-  };
-
-  const connectWalletConnect = () => {
-    wallet.connect("walletconnect");
-  };
-
-  const connectCoinbase = () => {
-    wallet.connect("walletlink");
+  const handleConnect = (connector) => {
+    connect(connector);
   };
 
   useEffect(() => {
-    if (wallet.account) {
-      onConnect && onConnect(wallet);
+    if (account) {
+      onConnect && onConnect();
       onClose && onClose();
     }
-  }, [wallet, onConnect, onClose]);
+  }, [account, onConnect, onClose]);
 
   return (
     <Modal visible={visible} onClose={onClose}>
@@ -41,7 +33,7 @@ function ConnectModal({
           wide
           label={'Metamask'}
           icon={<img src={`./wallets/metamask-fox.svg`} style={{ height: 24 }} alt="Metamask"/>}
-          onClick={connectMetamask}
+          onClick={() => {handleConnect('injected')}}
         />
       </div>
       <div style={{width: '50%', margin: 'auto', padding: '1%'}}>
@@ -49,7 +41,7 @@ function ConnectModal({
           wide
           label={'WalletConnect'}
           icon={<img src={`./wallets/wallet-connect.svg`} style={{ height: 24 }} alt="WalletConnect"/>}
-          onClick={connectWalletConnect}
+          onClick={() => {handleConnect('walletconnect')}}
         />
       </div>
       <div style={{width: '50%', margin: 'auto', padding: '1%'}}>
@@ -57,7 +49,7 @@ function ConnectModal({
           wide
           label={'Coinbase Wallet'}
           icon={<img src={`./wallets/coinbase-wallet.png`} style={{ height: 24 }} alt="Coinbase Wallet"/>}
-          onClick={connectCoinbase}
+          onClick={() => {handleConnect('walletlink')}}
         />
       </div>
     </Modal>
