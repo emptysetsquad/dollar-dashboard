daf456fa988b552cca694894542a90005455faa3
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-01-25 10:44:35 +0100

    Make sure all common components are exported consistently

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,25 +1,35 @@
 import AddressBlock from './AddressBlock';
 import BalanceBlock from './BalanceBlock';
+import BigNumberInput from './BigNumberInput';
+import CheckBox from './CheckBox';
+import Comment from './Comment';
+import EpochBlock from './EpochBlock'
+import HelperText from './HelpText';
+import IconHeader from './IconHeader';
+import MaxButton from './MaxButton';
+import NumberBlock from './NumberBlock';
+import PriceSection from './PriceSection';
 import RatioTag from './RatioTag';
 import SectionTitle from './SectionTitle';
-import MaxButton from './MaxButton';
-import HelperText from './HelpText';
-import PriceSection from './PriceSection';
+import TextBlock from './TextBlock';
 import WarningText from './Warning';
-import Comment from './Comment';
 import Wrapper from './Wrapper';
-import CheckBox from './CheckBox';
 
 export {
   AddressBlock,
   BalanceBlock,
+  BigNumberInput,
   CheckBox,
+  Comment,
+  EpochBlock,
+  HelperText,
+  IconHeader,
+  MaxButton,
+  NumberBlock,
+  PriceSection,
   RatioTag,
   SectionTitle,
-  MaxButton,
-  HelperText,
-  PriceSection,
-  Comment,
+  TextBlock,
   Wrapper,
   WarningText,
 };
