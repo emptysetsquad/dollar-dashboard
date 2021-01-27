84951350e6c937a442faa7dd9fe50ae6a74d80c1
Lewis Freiberg <freiberg.lewis@gmail.com>
2021-01-27 13:00:42 +0100

    Add Global styled and correct fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,71 @@
 import React, {useEffect, useState} from 'react';
+import styled, {createGlobalStyle} from 'styled-components'
 
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Main, Layout } from '@aragon/ui';
 import { UseWalletProvider } from 'use-wallet';
 import { updateModalMode } from './utils/web3';
 import { storePreference, getPreference } from './utils/storage';
+
 import NavBar from './components/NavBar';
-import HomePage from './pages/HomePage';
-import Trade from './pages/Trade/index';
 import Footer from './components/Footer';
-import Wallet from "./pages/Wallet";
-import EpochDetail from "./pages/EpochDetail";
-import CouponMarket from "./pages/CouponMarket";
-import Governance from "./pages/Governance";
-import Candidate from "./pages/Candidate";
-import Regulation from "./pages/Regulation";
-import Pool from "./pages/Pool";
+import HomePage from './pages/HomePage';
 import HomePageNoWeb3 from "./pages/HomePageNoWeb3";
+// import Trade from './pages/Trade/index';
+// import Wallet from "./pages/Wallet";
+// import EpochDetail from "./pages/EpochDetail";
+// import CouponMarket from "./pages/CouponMarket";
+// import Governance from "./pages/Governance";
+// import Candidate from "./pages/Candidate";
+// import Regulation from "./pages/Regulation";
+// import Pool from "./pages/Pool";
+
+const GlobalStyles = createGlobalStyle`
+   @font-face {
+    font-family: 'HKGrotesk';
+    font-style: normal;
+    font-weight: 700;
+    font-display: block;
+    src: url(fonts/HKGrotesk-Bold.otf)
+      format('opentype');
+  }
+   @font-face {
+    font-family: 'HKGrotesk';
+    font-style: normal;
+    font-weight: 400;
+    font-display: block;
+    src: url(fonts/HKGrotesk-Regular.otf)
+      format('opentype');
+  }
+   @font-face {
+    font-family: 'HKGrotesk';
+    font-style: normal;
+    font-weight: 300;
+    font-display: block;
+    src: url(fonts/HKGrotesk-Light.otf)
+      format('opentype');
+  }
+
+  body {
+    margin: 0;
+    font-family: 'HKGrotesk', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: black;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+  a {
+    text-decoration: none;
+    color: default;
+    :visited {
+      color: black;
+    }
+  }
+`
 
 function App() {
   const storedTheme = getPreference('theme', 'light');
@@ -63,15 +112,17 @@ function App() {
           }
         }}
       >
-        <Main assetsUrl={`${process.env.PUBLIC_URL}/aragon-ui/`} theme={theme} layout={false}>
+      <GlobalStyles/>
+        {/* <Main assetsUrl={`${process.env.PUBLIC_URL}/aragon-ui/`} theme={theme} layout={false}> */}
+        <Content>
           <NavBar hasWeb3={hasWeb3} />
-          <Layout>
+          {/* <Layout> */}
           {
             hasWeb3 ?
               <Switch>
-                <Route path="/dao/:override"><Wallet /></Route>
-                <Route path="/dao/"><Wallet /></Route>
-                <Route path="/epoch/"><EpochDetail /></Route>
+                {/* <Route path="/dao/:override"><Wallet /></Route>
+                <Route path="/dao/"><Wallet /></Route> */}
+                {/* <Route path="/epoch/"><EpochDetail /></Route>
                 <Route path="/coupons/:override"><CouponMarket /></Route>
                 <Route path="/coupons/"><CouponMarket /></Route>
                 <Route path="/governance/candidate/:candidate"><Candidate /></Route>
@@ -79,7 +130,7 @@ function App() {
                 <Route path="/trade/"><Trade /></Route>
                 <Route path="/regulation/"><Regulation /></Route>
                 <Route path="/pool/:override"><Pool /></Route>
-                <Route path="/pool/"><Pool /></Route>
+                <Route path="/pool/"><Pool /></Route> */}
                 <Route path="/"><HomePage /></Route>
               </Switch>
               :
@@ -87,14 +138,20 @@ function App() {
                 <Route path="/"><HomePageNoWeb3/></Route>
               </Switch>
           }
-          </Layout>
-          <div style={{height: '128px', width: '100%'}}/>
-          <Footer hasWeb3={hasWeb3} theme={theme} updateTheme={updateTheme}/>
-        </Main>
+          {/* </Layout> */}
+          <Footer />
+        </Content>
+        {/* </Main> */}
       </UseWalletProvider>
     </Router>
   );
 }
 
+const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`
 
 export default App;
